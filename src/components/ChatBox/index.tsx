aef2ec2708e8b287756fc557029c4d20d3e286b5
Tsaikkari <kirsi.trospe@gmail.com>
2021-02-06 13:48:02 +0100

    Modifying chatbox

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -1,4 +1,4 @@
-import React /*, { useState }*/ from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import Incoming from './Incoming'
@@ -9,21 +9,47 @@ import { AppState } from '../../redux/types'
 import './ChatBox.scss'
 
 const ChatBox = () => {
-  //const [newMessage, setNewMessage] = useState('')
-
+  // const role = useSelector((state: AppState) => state.user.userInfo.role)
+  // const companyName = useSelector((state: AppState) => state.employer.companyName)
+  // const jobseeker = useSelector((state: AppState) => state.jobseeker)
   const user = useSelector((state: AppState) => state.user)
 
-  const sendersMessage = {
-    sender: 'Kirsi',
-    receiver: 'Dilippo',
-    message: 'Hi Dilippo. We may have a job for you.',
-    time: '08:55',
+  const [newMessage, setNewMessage] = useState('')
+  // const [sender, setSender] = useState('')
+  // const [receiver, setReceiver] = useState('')
+  const time = new Date().toLocaleTimeString()
+
+  // if (role === 'employer') {
+  //   setSender(companyName)
+  //   setReceiver(jobseeker.firstName + ' ' + jobseeker.lastName)
+  // } else {
+  //   setSender(jobseeker.firstName + ' ' + jobseeker.lastName)
+  //   setReceiver(companyName)
+  // }
+
+  const [message, setMessage] = useState({
+    // sender: sender,
+    // receiver: receiver,
+    message: newMessage,
+    time: time,
+  })
+
+  const handleNewMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let message = e.target.value
+    setNewMessage(message)
+    console.log('message', message)
   }
-  const receiversMessage = {
-    sender: 'Dilippo',
-    reveicer: 'Kirsi',
-    message: "Hi Kirsi. I'm interested.",
-    time: '08:56',
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    console.log('etargetvalue', e.target.value)
+    console.log('time', time)
+    setMessage({
+      // sender: message.sender,
+      // receiver: message.receiver,
+      message: newMessage,
+      time: time,
+    })
   }
 
   return (
@@ -48,31 +74,29 @@ const ChatBox = () => {
           <div className="col-xl-8 col-sm-9 col-9">
             <div className="selected-user">
               <span>
-                To: <span className="chat-name">{sendersMessage.receiver}</span>
+                {/* To: <span className="chat-name">{receiver}</span> */}
               </span>
             </div>
             <div className="chat-container chat-messages">
               <ul className="chat-box">
-                <Outgoing sendersMessage={sendersMessage} />
-                <Incoming receiversMessage={receiversMessage} />
+                <Outgoing sendersMessage={message} />
+                <Incoming receiversMessage={message} />
               </ul>
             </div>
             <div className="flex-grow-0 py-3 px-4 border-top">
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  // value={newMessage}
-                  // onChange={e => handleNewMessageChange(e)}
-                  placeholder="Write message..."
-                />
-                <button
-                  // onClick={handleSendMessage}
-                  className="btn btn-primary"
-                >
-                  Send
-                </button>
-              </div>
+              <form onSubmit={(e: any) => handleSubmit(e)}>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="message"
+                    value={newMessage}
+                    onChange={handleNewMessageChange}
+                    placeholder="Write message..."
+                  />
+                </div>
+              </form>
+              <button className="btn btn-primary">Send</button>
             </div>
           </div>
         </div>
@@ -80,4 +104,5 @@ const ChatBox = () => {
     </>
   )
 }
+
 export default ChatBox
